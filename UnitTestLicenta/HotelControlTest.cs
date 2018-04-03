6d0e96e08ca74b02596091f889b6b60c91d15997
DiaSergiu <diasergiu2@gmail.com>
2018-04-03 15:48:14 +0300

    add UnitTest that dose nothing at the moment

---
+++ UnitTestLicenta/HotelControlTest.cs
@@ -0,0 +1,24 @@
+﻿using Licenta.Controlers;
+using Licenta.Entityes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestLicenta
+{
+    [TestClass]
+    class HotelControlTest
+    {
+        DBRezervareHotelieraContext context = new DBRezervareHotelieraContext();
+
+        [TestMethod]
+        public void AddToTheDataBase()
+        {
+            HotelsController hotel = new HotelsController(context);
+            //await hotel.AddHotel();
+
+
+        } 
+    }
+}
